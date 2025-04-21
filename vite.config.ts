56baf12a83e4735821a85df0dd24d0a27efdafa1
AlassiriMA <alassirimail@gmail.com>
2025-04-21 13:27:07 +0200

    Repr_ commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend_protfolio/',
+  base: '/frontend_portfolio/',
   plugins: [react()],
 })
